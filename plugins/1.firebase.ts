160c08b51f9c34f8326497c894f40f7a262caba1
xyytgae <xyytgae@yahoo.co.jp>
2023-02-18 00:30:22 +0900

    feat: #10 🎸 nuxt3用にpluginsを修正

---
+++ plugins/1.firebase.ts
@@ -0,0 +1,32 @@
+// NOTE: pluginsの登録順の都合により、このファイルの先頭に1.を設定
+// 別pluginsで使用するためこのファイルを最初に登録する必要あり
+// https://nuxt.com/docs/guide/directory-structure/plugins#plugin-registration-order
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+import { FirebaseFirestore } from '@firebase/firestore-types'
+import { FirebaseAuth } from '@firebase/auth-types'
+import { FirebaseStorage } from '@firebase/storage-types'
+import config from '../firebaseConfig.json'
+import { defineNuxtPlugin } from '#app'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({ ...config })
+}
+
+export default defineNuxtPlugin((nuxtApp) => {
+  // nuxtApp.provide('firebase', firebase)
+  nuxtApp.provide('firestore', firebase.firestore())
+  nuxtApp.provide('fireAuth', firebase.auth())
+  nuxtApp.provide('fireStorage', firebase.storage())
+})
+
+declare module '#app' {
+  interface NuxtApp {
+    $firestore: FirebaseFirestore
+    $fireAuth: FirebaseAuth
+    $fireStorage: FirebaseStorage
+  }
+}
