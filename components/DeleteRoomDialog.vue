87d013ad4a97fee2951e8136770fdf3dfd92335f
xyytgae <xyytgae@yahoo.co.jp>
2023-05-13 16:54:35 +0900

    feat: #68 🎸 Vuetify3に合わせて修正

--- components/DeleteRoomDialog.vue
+++ components/DeleteRoomDialog.vue
@@ -7,14 +7,18 @@ const dialog = ref(true)
 <template>
   <v-dialog v-model="dialog" max-width="300px">
     <v-card>
-      <v-card-title> ゲームを終了 </v-card-title>
+      <v-card-item>
+        <v-card-title> ゲームを終了 </v-card-title>
+      </v-card-item>
       <v-card-text> ルームを退出しますか？ </v-card-text>
       <v-card-actions>
-        <v-btn @click="$emit('close-dialog')" color="info" text
+        <v-btn variant="text" color="info" @click="$emit('close-dialog')"
           >キャンセル</v-btn
         >
         <v-spacer></v-spacer>
-        <v-btn @click="$emit('delete-room')" color="info" text>OK</v-btn>
+        <v-btn variant="text" color="info" @click="$emit('delete-room')"
+          >OK</v-btn
+        >
       </v-card-actions>
     </v-card>
   </v-dialog>
