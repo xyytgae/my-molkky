0e6da6ef464cb182d4d35b434aeb2b7e09aa1124
xyytgae <xyytgae@yahoo.co.jp>
2023-05-27 22:15:48 +0900

    feat: #105 🎸 storeとpluginに取って代わってcomposableを実装

---
+++ composables/useUser.ts
@@ -0,0 +1,137 @@
+import { useState, useNuxtApp } from '#app'
+
+// TODO: 型修正
+type ApiResponse = {
+  success: boolean
+  error: any
+}
+
+// TODO: 型修正
+type UserState = {
+  name: string
+  iconImageUrl: string
+  stars: number
+  uid: string
+}
+
+export const useUser = () => {
+  const { $firebase, $fireAuth, $firestore } = useNuxtApp()
+  const loginedUser = useState<UserState | null>('loginedUser', () => null)
+
+  const getUser = async (uid: string): Promise<ApiResponse> => {
+    try {
+      const userSnapshot = await $firestore.collection('users').doc(uid).get()
+      if (userSnapshot.exists) {
+        loginedUser.value = { ...userSnapshot.data(), uid } as UserState
+      } else {
+        loginedUser.value = {
+          name: '',
+          iconImageUrl: '',
+          stars: 0,
+          uid,
+        }
+      }
+
+      return {
+        success: true,
+        error: null,
+      }
+    } catch (error) {
+      return {
+        success: false,
+        error,
+      }
+    }
+  }
+
+  const googleLogin = async (): Promise<ApiResponse> => {
+    try {
+      const googleAuthProvider = new $firebase.auth.GoogleAuthProvider()
+      const userCredential = await $fireAuth.signInWithPopup(googleAuthProvider)
+      if (userCredential && userCredential.user) {
+        await getUser(userCredential.user.uid)
+      }
+
+      return {
+        success: true,
+        error: null,
+      }
+    } catch (error) {
+      return {
+        success: false,
+        error,
+      }
+    }
+  }
+
+  const guestLogin = async (
+    email: string,
+    password: string
+  ): Promise<ApiResponse> => {
+    try {
+      const userCredential = await $fireAuth.signInWithEmailAndPassword(
+        email,
+        password
+      )
+      if (userCredential && userCredential.user) {
+        await getUser(userCredential.user.uid)
+      }
+
+      return {
+        success: true,
+        error: null,
+      }
+    } catch (error) {
+      return {
+        success: false,
+        error,
+      }
+    }
+  }
+
+  const logout = async (): Promise<ApiResponse> => {
+    try {
+      await $fireAuth.signOut()
+      loginedUser.value = null
+
+      return {
+        success: true,
+        error: null,
+      }
+    } catch (error) {
+      return {
+        success: false,
+        error,
+      }
+    }
+  }
+
+  const checkAuthState = async (): Promise<void> => {
+    return await new Promise<void>((resolve, reject) => {
+      $fireAuth.onAuthStateChanged(
+        async (user) => {
+          if (user) {
+            const { uid } = user
+            await getUser(uid)
+            resolve()
+          } else {
+            loginedUser.value = null
+            resolve()
+          }
+        },
+        (error) => {
+          reject(error)
+        }
+      )
+    })
+  }
+
+  return {
+    loginedUser,
+    getUser,
+    googleLogin,
+    guestLogin,
+    logout,
+    checkAuthState,
+  }
+}
--- store/main.ts
+++ store/main.ts
@@ -1,9 +1,7 @@
 import { defineStore } from 'pinia'
-import { useNuxtApp, useRouter } from '#app'
+import { useNuxtApp } from '#app'
 
 interface State {
-  login_user: any | null
-  registered_user: any | null
   gameHistoryCreatedAt: Array<any>
   gameHistoryUsers: Array<any>
   userData: Array<any>
@@ -11,62 +9,15 @@ interface State {
 
 export const useMainStore = defineStore('main', {
   state: (): State => ({
-    login_user: null,
-    registered_user: null,
     gameHistoryCreatedAt: [],
     gameHistoryUsers: [],
     userData: [],
   }),
   getters: {
-    getterLogin_user: (state) => state.login_user,
-    getterRegistered_user: (state) => state.registered_user,
     getterGameHistoryCreatedAt: (state) => state.gameHistoryCreatedAt,
     getterGameHistoryUsers: (state) => state.gameHistoryUsers,
   },
   actions: {
-    async guestLogin({ email, password }) {
-      await useNuxtApp()
-        .$fireAuth.signInWithEmailAndPassword(email, password)
-        .then(() => {
-          // this.$router.push('/')
-          useRouter().push('/')
-          // this.$router.push('/')
-        })
-    },
-    async login() {
-      const { $firebase, $fireAuth } = useNuxtApp()
-      const google_auth_provider = new $firebase.auth.GoogleAuthProvider()
-      try {
-        await $fireAuth.signInWithPopup(google_auth_provider)
-      } catch (error) {
-        console.log(error)
-      }
-      useRouter().push('/rooms')
-
-      // NOTE: バグなのかログイン後にリダレクトできないため不使用
-      // $fireAuth.signInWithRedirect(google_auth_provider)
-    },
-    logout() {
-      useNuxtApp().$fireAuth.signOut()
-    },
-    setLoginUser({ uid, displayName, email }) {
-      this.login_user = { uid, displayName, email }
-    },
-    deleteLoginUser() {
-      this.login_user = null
-    },
-    getRegisteredUser({ uid }) {
-      useNuxtApp()
-        .$firestore.collection('users')
-        .doc(uid)
-        .get()
-        .then((doc) => {
-          const docData = doc.data()
-          const { name, iconImageUrl, stars } = docData
-          this.registered_user = { name, iconImageUrl, stars }
-        })
-    },
-
     getGameHistory({ userId }) {
       useNuxtApp()
         .$firestore.collection('users')
