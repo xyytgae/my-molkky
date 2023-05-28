218e4faae08f8359983061f0bdefff358d4c4235
xyytgae <xyytgae@yahoo.co.jp>
2023-05-29 03:28:39 +0900

    feat: #38 🎸 storeで状態管理をやめる

---
+++ composables/useRooms.ts
@@ -0,0 +1,78 @@
+import { useState, useNuxtApp } from '#app'
+import { Room, ApiResponse } from '../types/api'
+import { readonly } from '#imports'
+import { Unsubscribe } from 'firebase'
+
+const add = (rooms: Room[], addedRoom: Room): Room[] => {
+  const isNotAdded = !rooms.find((room) => room.id === addedRoom.id)
+  if (isNotAdded) {
+    rooms.push(addedRoom)
+  }
+  return rooms
+}
+
+const update = (rooms: Room[], updatedRoom: Room): Room[] => {
+  return rooms.map((room) => {
+    if (room.id === updatedRoom.id) {
+      room = updatedRoom
+    }
+    return room
+  })
+}
+
+const remove = (rooms: Room[], removedRoom: Room): Room[] => {
+  return rooms.filter((room) => room.id !== removedRoom.id)
+}
+
+export const useRooms = () => {
+  const { $firestore } = useNuxtApp()
+  const rooms = useState<Room[]>('rooms', () => [])
+
+  const subscribe = async (): Promise<ApiResponse<Unsubscribe | null>> => {
+    try {
+      rooms.value = []
+      const unsubscribe = await $firestore
+        .collection('rooms')
+        .orderBy('createdAt', 'desc')
+        .onSnapshot((roomsSnapShot) => {
+          roomsSnapShot.docChanges().forEach((snapshot) => {
+            const room: Room = {
+              id: snapshot.doc.id,
+              ...(snapshot.doc.data() as Omit<Room, 'id'>),
+            }
+
+            switch (snapshot.type) {
+              case 'added':
+                rooms.value = add(rooms.value, room)
+                break
+
+              case 'modified':
+                rooms.value = update(rooms.value, room)
+                break
+
+              case 'removed':
+                rooms.value = remove(rooms.value, room)
+                break
+            }
+          })
+        })
+
+      return {
+        data: unsubscribe,
+        success: true,
+        error: null,
+      }
+    } catch (error) {
+      return {
+        data: null,
+        success: false,
+        error,
+      }
+    }
+  }
+
+  return {
+    rooms: readonly(rooms),
+    subscribe,
+  }
+}
--- types/api.ts
+++ types/api.ts
@@ -10,3 +10,18 @@ export type GameHistory = {
   createdAt: firestore.Timestamp
   users: Array<any>
 }
+
+export type Room = {
+  id: string
+  hostId: string
+  name: string
+  password: string
+  topImageUrl: string
+  createdAt: firestore.Timestamp
+  startSecondHalf: boolean
+  startFirstHalf: boolean
+  finishFirstHalf: boolean
+  finishSecondHalf: boolean
+  delete: boolean
+  users: Array<string>
+}
