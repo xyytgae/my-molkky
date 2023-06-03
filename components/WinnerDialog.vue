e182ac99bfb307d27b0a2dc0b130ef69e1681bbe
xyytgae <xyytgae@yahoo.co.jp>
2023-06-04 01:43:06 +0900

    feat: #67 🎸 script setupに修正

--- components/WinnerDialog.vue
+++ components/WinnerDialog.vue
@@ -1,3 +1,47 @@
+<script setup lang="ts">
+import { ref, onMounted } from '#imports'
+
+const dialog = ref<boolean>(true)
+const showChampion = ref<boolean>(false)
+const showBorders = ref<boolean>(false)
+const showText = ref<boolean>(false)
+
+const timer = ref<NodeJS.Timer | null>(null)
+const time = ref<number>(0)
+
+const clickScreen = () => {
+  dialog.value = false
+}
+
+const stopTimer = () => {
+  if (timer.value) {
+    clearInterval(timer.value)
+    time.value = 0
+    timer.value = null
+  }
+}
+
+onMounted(() => {
+  timer.value = setInterval(() => {
+    time.value++
+    switch (time.value) {
+      case 1:
+        showChampion.value = true
+        break
+
+      case 2:
+        showText.value = true
+        break
+
+      case 3:
+        showBorders.value = true
+        break
+    }
+    if (time.value >= 3) stopTimer()
+  }, 1000)
+})
+</script>
+
 <template>
   <v-dialog v-model="dialog" fullscreen>
     <v-card class="text-center" color="black" @click="clickScreen">
@@ -37,54 +81,6 @@
     </v-card>
   </v-dialog>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      dialog: true,
-      showChampion: false,
-      showBorders: false,
-      showText: false,
-
-      timer: null,
-      time: 0,
-    }
-  },
-  mounted() {
-    this.timer = setInterval(() => {
-      this.time++
-      switch (this.time) {
-        case 1:
-          this.showChampion = true
-          break
-
-        case 2:
-          this.showText = true
-          break
-
-        case 3:
-          this.showBorders = true
-          break
-      }
-      if (this.time >= 3) this.stopTimer()
-    }, 1000)
-  },
-  methods: {
-    clickScreen() {
-      this.dialog = false
-    },
-
-    stopTimer() {
-      if (this.timer) {
-        clearInterval(this.timer)
-        this.time = 0
-        this.timer = 0
-      }
-    },
-  },
-}
-</script>
 
 <style scoped>
 .main {
