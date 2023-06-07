770f618f85850c2130b43b025381c349a20816c0
xyytgae <xyytgae@yahoo.co.jp>
2023-06-08 01:09:46 +0900

    refactor: #148 💡 pages/profileのリファクタ

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,97 +1,66 @@
 <script setup lang="ts">
-import { useNuxtApp, useRouter } from '#app'
+import { useRouter } from '#app'
 import { definePageMeta, ref, reactive, useUser } from '#imports'
+import { onSelectFile } from '~/modules/onSelectFile'
+import { userRepository } from '~/apis/user'
+import { storageRepository } from '~/apis/storage'
+
+type FormInputs = {
+  name: string
+  image: string
+}
 
 definePageMeta({
   middleware: ['check-auth'],
 })
+
 const router = useRouter()
-const { $firestore, $fireStorage } = useNuxtApp()
 const { loginedUser } = useUser()
 
-const image = ref<HTMLInputElement | null>(null)
-const form = reactive<{
-  name: {
-    label: string
-    value: string | null
-  }
-  image: {
-    label: string
-    value: string | null
-  }
-  stars: {
-    label: string
-    value: number
-  }
-}>({
-  name: {
-    label: 'プレイヤー名',
-    value: null,
-  },
-  image: {
-    label: 'アイコン画像',
-    value: null,
-  },
-  stars: {
-    label: 'スター',
-    value: 0,
-  },
+const starCount = ref<number>(0)
+const formInputs = reactive<FormInputs>({
+  name: '',
+  image: '',
 })
+const inputRef = ref<HTMLInputElement | null>(null)
 
 const onSubmit = async () => {
-  try {
-    await $firestore.collection('users').doc(loginedUser.value!.id).update({
-      name: form.name.value,
-      iconImageUrl: form.image.value,
-      // stars: form.stars.value,
-    })
-    router.push('/rooms')
-  } catch (e) {
-    console.log('失敗しました')
-    console.log(e)
+  const input = {
+    name: formInputs.name,
+    iconImageUrl: formInputs.image,
+    stars: starCount.value,
   }
+  await userRepository.update({
+    userId: loginedUser.value!.id,
+    input,
+  })
+  router.push('/rooms')
 }
 
 const selectImage = () => {
-  if (image.value) {
-    image.value.click()
+  if (inputRef.value) {
+    inputRef.value.click()
   }
 }
 
-const onSelectFile = (e: Event) => {
-  if (!(e.target instanceof HTMLInputElement)) {
-    return
-  }
-  const files = e.target.files
-  if (files === null || files.length === 0) return
-
-  const reader = new FileReader()
-  reader.readAsDataURL(files[0])
-
-  reader.addEventListener('load', () => {
-    upload({
-      localImageFile: files[0],
+const uploadImage = async (event: Event) => {
+  const localImageFile = await onSelectFile(event)
+  if (localImageFile) {
+    const path = `${loginedUser.value!.id}/${localImageFile.name}`
+    const { data } = await storageRepository.upload({
+      file: localImageFile,
+      path,
     })
-  })
-}
-
-const upload = async ({ localImageFile }: { localImageFile: File }) => {
-  const storageRef = $fireStorage.ref()
-
-  const imageRef = storageRef.child(
-    `images/${loginedUser.value!.id}/${localImageFile.name}`
-  )
-
-  const snapShot = await imageRef.put(localImageFile)
-  form.image.value = await snapShot.ref.getDownloadURL()
+    formInputs.image = data
+  }
 }
 
 /**
  * init
  */
-form.name.value = loginedUser.value!.name
-form.image.value = loginedUser.value!.iconImageUrl
-form.stars.value = loginedUser.value!.stars
+formInputs.name = loginedUser.value!.name
+formInputs.image = loginedUser.value!.iconImageUrl
+starCount.value = loginedUser.value!.stars
 </script>
 
 <template>
@@ -104,32 +73,25 @@ form.stars.value = loginedUser.value!.stars
       <v-container class="d-flex justify-center align-center">
         <form class="form" @submit.prevent="onSubmit">
           <div class="image">
-            <template v-if="form.image.value">
-              <img class="icon" :src="form.image.value" @click="selectImage" />
-
-              <!-- <v-avatar size="150" class="image">
-            <img :src="form.image.value" @click="selectImage" />
-          </v-avatar> -->
+            <template v-if="formInputs.image">
+              <img class="icon" :src="formInputs.image" @click="selectImage" />
             </template>
             <template v-else>
-              <!-- <v-avatar class="image" size="180">
-            <img :src="getterLogin_user.photoURL" />
-          </v-avatar>-->
               <v-icon color="gray" size="150" @click="selectImage"
                 >mdi-account-circle</v-icon
               >
             </template>
             <input
-              ref="image"
+              ref="inputRef"
               type="file"
               style="display: none"
               accept="image/*"
-              @change="onSelectFile"
+              @change="uploadImage"
             />
           </div>
           <div>
             <v-text-field
-              v-model="form.name.value"
+              v-model="formInputs.name"
               label="プレイヤー名"
               class="mt-2"
               counter="8"
@@ -140,7 +102,7 @@ form.stars.value = loginedUser.value!.stars
 
           <div class="star">
             <span style="color: #ffa000"> ★ </span>
-            ×{{ form.stars.value }}
+            ×{{ starCount }}
           </div>
 
           <div class="button">
