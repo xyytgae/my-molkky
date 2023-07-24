00f452788acfe4ac8554635b7b2db03c2e9b9273
xyytgae <xyytgae@yahoo.co.jp>
2023-07-24 22:49:33 +0900

    feat: #198 🎸 UIを変更

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from '#app'
+import { mdiAccount, mdiHome } from '@mdi/js'
 import { definePageMeta, ref, useUser } from '#imports'
 
 definePageMeta({
@@ -9,6 +10,7 @@ const { googleLogin, guestLogin } = useUser()
 const router = useRouter()
 
 const isOpenedOverlay = ref<boolean>(false)
+const isDrawerOpen = ref<boolean>(false)
 
 const handleGuestLogin = async (email: string, password: string) => {
   isOpenedOverlay.value = true
@@ -28,59 +30,101 @@ const handleGoogleLogin = async () => {
 </script>
 
 <template>
-  <v-main>
-    <v-container class="d-flex justify-center align-center py-12">
-      <v-card class="mx-auto mt-5" width="350">
-        <!-- <v-container> -->
-        <v-card-title class="text-center my-3"> ログイン </v-card-title>
+  <div>
+    <v-navigation-drawer v-model="isDrawerOpen" app clipped>
+      <v-list>
+        <v-list-item to="/">
+          <template #prepend>
+            <v-icon :icon="mdiHome" color="black" />
+          </template>
+          <v-list-item-title class="text-subtitle-2 text-black"
+            >ホーム</v-list-item-title
+          >
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar flat app>
+      <template #prepend>
+        <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen" />
+      </template>
+    </v-app-bar>
 
-        <v-divider class="mx-7" />
+    <v-main>
+      <v-container class="d-flex justify-center align-center py-12">
+        <v-card class="mx-auto mt-5" elevation="0" width="350">
+          <!-- <v-container> -->
+          <v-card-title class="text-center my-3"> ログイン </v-card-title>
 
-        <v-card-actions>
-          <v-row class="mx-6">
-            <v-col class="text-center">
-              <v-btn
-                color="info"
-                variant="elevated"
-                size="large"
-                block
-                @click="handleGoogleLogin"
-                >Googleでログイン</v-btn
-              >
-            </v-col>
-            <v-col class="text-center">
-              <v-btn
-                color="primary"
-                variant="elevated"
-                size="large"
-                block
-                @click="handleGuestLogin('test1@example.com', '123456')"
-                >デモアカウント1でログイン</v-btn
-              >
-            </v-col>
-            <v-col class="text-center">
-              <v-btn
-                color="primary"
-                variant="elevated"
-                size="large"
-                block
-                @click="handleGuestLogin('test2@example.com', '123456')"
-                >デモアカウント2でログイン</v-btn
-              >
-            </v-col>
-          </v-row>
-        </v-card-actions>
-        <!-- </v-container> -->
-      </v-card>
+          <v-divider class="mx-7" />
 
-      <v-overlay
-        :model-value="isOpenedOverlay"
-        class="align-center justify-center"
-      >
-        <v-progress-circular color="primary" indeterminate size="128"
-          >ログイン中...</v-progress-circular
+          <v-card-actions class="py-10">
+            <v-row class="mx-4">
+              <v-col class="text-center">
+                <v-btn
+                  class="text-none"
+                  block
+                  variant="elevated"
+                  size="large"
+                  elevation="1"
+                  @click="handleGoogleLogin"
+                >
+                  <img id="google-icon" src="/google.png" />
+                  Googleでログイン</v-btn
+                >
+              </v-col>
+              <v-col class="text-center">
+                <v-btn
+                  variant="elevated"
+                  size="large"
+                  block
+                  elevation="1"
+                  @click="handleGuestLogin('test1@example.com', '123456')"
+                >
+                  <v-icon :icon="mdiAccount" size="24" />
+                  デモアカウント1でログイン</v-btn
+                >
+              </v-col>
+              <v-col class="text-center">
+                <v-btn
+                  variant="elevated"
+                  size="large"
+                  block
+                  elevation="1"
+                  @click="handleGuestLogin('test2@example.com', '123456')"
+                >
+                  <v-icon :icon="mdiAccount" size="24" />
+                  デモアカウント2でログイン</v-btn
+                >
+              </v-col>
+            </v-row>
+          </v-card-actions>
+          <!-- </v-container> -->
+        </v-card>
+
+        <v-overlay
+          :model-value="isOpenedOverlay"
+          class="align-center justify-center"
         >
-      </v-overlay>
-    </v-container>
-  </v-main>
+          <v-progress-circular color="primary" indeterminate size="128"
+            >ログイン中...</v-progress-circular
+          >
+        </v-overlay>
+      </v-container>
+    </v-main>
+  </div>
 </template>
+
+<style scoped>
+.v-app-bar {
+  border-bottom: 1px solid grey;
+}
+.v-main {
+  background-color: rgb(var(--v-theme-warm-vanilla));
+  height: 100vh;
+}
+#google-icon {
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
+}
+</style>
